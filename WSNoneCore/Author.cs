42479a6b45fbf211454c4398268e25df10402385
Wei Xia <wei_xia@hotmail.com>
2020-06-07 21:08:19 -0700

    Add project files.

---
+++ WSNoneCore/Author.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSNoneCore
+{
+    public class Author
+    {
+        public Author()
+        {
+            Books = new HashSet<Book>();
+        }
+
+        //[JsonPropertyName("id")]
+        public int Id { get; set; }
+        //[JsonPropertyName("firstName")]
+        public string FirstName { get; set; }
+        //[JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+       // [JsonPropertyName("age")]
+        public int Age { get; set; }
+       // [JsonPropertyName("country")]
+        public string Country { get; set; }
+        //[JsonPropertyName("books")]
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
---
+++ WSNoneCore/Program.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSNoneCore
+{
+    class Program
+    {
+        static HttpClientHandler handler = new HttpClientHandler()
+        {
+            UseDefaultCredentials = true
+        };
+
+        static HttpClient client = new HttpClient(handler);
+        //static HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            Author a = new Author();
+            a.Age = 16;
+            a.FirstName = "Kelly";
+            a.LastName = "System";
+            a.Country = "German";
+
+            var uriL = await CreateProductAsync(a);
+        }
+
+
+        static async Task<Uri> CreateProductAsync(Author author)
+        {
+
+
+           
+
+            //var stringContent = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+            //var response = await httpClient.PostAsync("http://www.sample.com/write", stringContent);
+
+
+            var stringAuthor = new StringContent(JsonConvert.SerializeObject(author), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(
+                "https://localhost:44388/api/authors", stringAuthor);
+            response.EnsureSuccessStatusCode();
+            var status = response.StatusCode;
+            var returnContent =await response.Content.ReadAsStringAsync();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+    }
+}
